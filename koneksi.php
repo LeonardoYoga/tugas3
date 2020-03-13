2fcfa2029c781fa98c47d64b03055dbe585f3f23
LeonardoYoga <61863201+LeonardoYoga@users.noreply.github.com>
2020-03-13 14:56:14 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+?php
+define('HOST' , 'localhost');
+define('USER' , 'id306496 praktikum');
+define('PASS' , '123456');
+define('DB' , 'id306496_praktikum);
+
+$con = mysqli_connect{HOST,USER,PASS,DB} or die{'unable to connect');
